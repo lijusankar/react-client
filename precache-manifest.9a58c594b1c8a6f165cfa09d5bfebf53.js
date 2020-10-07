3068425dac4047bc0414e7c2b698a9ac2b64c715
lijayaku <lijayaku@amazon.com>
2020-10-06 17:00:58 -0700

    Updates

---
+++ precache-manifest.9a58c594b1c8a6f165cfa09d5bfebf53.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea1e8dee221fe786a794de64964e2150",
+    "url": "/react-client/index.html"
+  },
+  {
+    "revision": "6c95bba22ff8886471a2",
+    "url": "/react-client/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b82c2d8a16f3f0dff05d",
+    "url": "/react-client/static/js/2.987f812b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-client/static/js/2.987f812b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6c95bba22ff8886471a2",
+    "url": "/react-client/static/js/main.230a7423.chunk.js"
+  },
+  {
+    "revision": "b897f2aeee927ca02675",
+    "url": "/react-client/static/js/runtime-main.047b3da7.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-client/static/media/logo.5d5d9eef.svg"
+  }
+]);
